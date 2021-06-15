b9164fc79c45454f124c977b850520ae0de14be6
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-06-15 16:39:38 -0300

    feat: add BaseService

---
+++ src/MyAPI.Business/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MyAPI.Business.Models;
+
+namespace MyAPI.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected static bool IsValid<TV, TE>(TV validacao, TE entidade)
+            where TV : AbstractValidator<TE>
+            where TE : Entity
+        {
+            return validacao.Validate(entidade).IsValid;
+        }
+    }
+}
