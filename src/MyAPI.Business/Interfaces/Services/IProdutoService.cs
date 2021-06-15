9bd2e2c541af4e1eb1f00d8b2b44e9fa9030e830
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-06-15 16:40:32 -0300

    feat: add ProdutoService

---
+++ src/MyAPI.Business/Interfaces/Services/IProdutoService.cs
@@ -0,0 +1,11 @@
+﻿using MyAPI.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MyAPI.Business.Interfaces.Services
+{
+    public interface IProdutoService : IDisposable
+    {
+        Task Adicionar(Produto produto);
+    }
+}
---
+++ src/MyAPI.Business/Services/ProdutoService.cs
@@ -0,0 +1,32 @@
+﻿using MyAPI.Business.Interfaces.Repositories;
+using MyAPI.Business.Interfaces.Services;
+using MyAPI.Business.Models;
+using MyAPI.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace MyAPI.Business.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        private readonly IProdutoRepository _repository;
+
+        public ProdutoService(IProdutoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Adicionar(Produto produto)
+        {
+            if (!IsValid(new ProdutoValidation(), produto)) return;
+
+            await _repository.Adicionar(produto);
+        }
+
+        public void Dispose()
+        {
+            _repository?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
