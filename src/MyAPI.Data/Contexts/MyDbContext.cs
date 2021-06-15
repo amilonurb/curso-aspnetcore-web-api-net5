d4382cbadd41eaf19d6f0e27078d34b0b556d047
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-06-15 17:58:45 -0300

    feat: add db context configs

--- src/MyAPI.Data/Contexts/MyDbContext.cs
+++ src/MyAPI.Data/Contexts/MyDbContext.cs
@@ -1,17 +1,59 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MyAPI.Data.Contexts
 {
     public class MyDbContext : DbContext
     {
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            if (Database.GetPendingMigrations().Any())
+            {
+                Database.Migrate();
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                                                       .SelectMany(e => e.GetProperties()
+                                                                         .Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetMaxLength(100);
+            }
+
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                                                           .SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries()
+                                               .Where(e => e.Entity.GetType().GetProperty("DataCadastro") is not null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
