a6c4e3c7de3d65fbc08aa3e0e2b7a1e29a510da3
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-06-15 16:41:34 -0300

    feat: add AutoMapper config

---
+++ src/MyAPI/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MyAPI.Business.Models;
+using MyAPI.ViewModels;
+
+namespace MyAPI.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<ProdutoViewModel, Produto>().ReverseMap();
+        }
+    }
+}
