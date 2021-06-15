f7727859657ed3e37d18bc3e96d2ef9aa7053774
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-06-15 16:41:15 -0300

    feat: add controller de produtos

---
+++ src/MyAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MyAPI.Business.Interfaces.Services;
+using MyAPI.Business.Models;
+using MyAPI.ViewModels;
+using System.Threading.Tasks;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("produtos")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly IProdutoService _service;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(IProdutoService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+
+            var produto = _mapper.Map<Produto>(produtoViewModel);
+
+            await _service.Adicionar(produto);
+
+            return Created("", null);
+        }
+    }
+}
